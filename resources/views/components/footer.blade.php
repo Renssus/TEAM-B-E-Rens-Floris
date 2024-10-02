35cd25ab9d9b7f268ba49780b49d303c856d773d
Floris29 <Florismelchers@gmail.com>
2024-10-02 14:19:05 +0200

    ticket 5 fix

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,27 +1,23 @@
-<footer class="bg-dark text-white p-6">
-    <!-- Main Container -->
-    <div class="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 text-center md:text-left">
+<footer class="bg-dark text-white">
 
+    <div class="d-flex container text-center justify-content-around">
+        <div class="mb-4">
+            <h3 class="text-lg font-semibold ">Contactgegevens</h3>
+            <ul class="mt-2 list-unstyled">
+                <li>Email: info@bedrijf.nl</li>
+                <li>Telefoon: 123-456-7890</li>
+                <li>Adres: Straatnaam 1, 1234 AB Stad</li>
+            </ul>
+        </div>
 
-        <div class="d-flex">
-            <div class="mb-4">
-                <h3 class="text-lg font-semibold ">Contactgegevens</h3>
-                <ul class="mt-2 list-unstyled">
-                    <li>Email: info@bedrijf.nl</li>
-                    <li>Telefoon: 123-456-7890</li>
-                    <li>Adres: Straatnaam 1, 1234 AB Stad</li>
-                </ul>
-            </div>
+        <div class="mb-4 ">
+            <h3 class="text-lg font-semibold">Over ons</h3>
+            <p class="mt-2">
+                Wij zijn Avarix
+            </p>
+        </div>
 
-            <div class="mb-4">
-              <h3 class="text-lg font-semibold">Over ons</h3>
-              <p class="mt-2">
-                  Wij zijn een team van gepassioneerde webontwikkelaars die zich richten op het leveren van hoogwaardige
-                  en moderne weboplossingen.
-              </p>
-          </div>
-
-          <div class="mb-4">
+        <div class="mb-4">
             <h3 class="text-lg font-semibold">Snelle links</h3>
             <ul class="mt-2 space-y-1 list-unstyled">
                 <li><a href="#">Home</a></li>
@@ -30,12 +26,6 @@
                 <li><a href="#">Contact</a></li>
             </ul>
         </div>
-
-        </div>
-
-
-        
-        <!-- About Us Section -->
 
     </div>
 
